b93d117795c831c246496400ebed9e2fc927e10d
Rezwan-Prince07 <demoprince610@gmail.com>
2026-07-16 23:10:54 +0600

    utils function and api response craeted

---
+++ src/types/apiResponse.ts
@@ -0,0 +1,20 @@
+export interface SuccessResponse<T> {
+  statusCode: number;
+  success: true;
+  message: string;
+  //meta?: PaginationMeta;
+  data?: T;
+}
+
+export interface ErrorDetails {
+  field?: string;
+  message: string;
+}
+
+export interface ErrorResponse {
+  statusCode: number;
+  success: false;
+  message: string;
+  errorDetails?: ErrorDetails[];
+  errorStack?: string;
+}
